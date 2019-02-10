b458dd125f4b8a3f0712d96a259e9b5b18b7ff12
nthmetal <832529@gmail.com>
2019-02-09 20:12:11 -0500

    small cleanup

--- src/bot.js
+++ src/bot.js
@@ -4,8 +4,6 @@ var {logger} = require('./logger/logger.js');
 var {env} = require(`./_config/env.js`);
 var eventHandler = require('./commands/eventHandler.js');
 var MongoClient = require('mongodb');
- 
-
 
 var gbot = new Discord.Client({
    token: auth.token,
@@ -16,10 +14,6 @@ var prefix = env.default_prefix;
 eventHandler.initialize(gbot, logger);
 eventHandler = eventHandler.eventHandler;
 
-
-
-
-
 gbot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
@@ -27,8 +21,6 @@ gbot.on('ready', function (evt) {
     logger.info('GBot is running in '+ env.ENV + 'environment');
     logger.debug('---------This should not be visible in production');
     gbot.setPresence({ status: 'online', game: { name: 'with @Lichaes' } });
-    // gbot.sendMessage({to: '537108393626697748', message: 'n!marry <@!101794991625490432>'})
-    // gbot.sendMessage({to: '537108393626697748', message: 'yes'})
 });
 
 console.log("connecting to gbot's brain");
@@ -43,29 +35,5 @@ MongoClient.connect(env.mongoURL, function(err, client) {
 
     gbot.on('any', (event) => eventHandler(event, prefix, gbot, db));
    
-    // client.close();
+    client.close();
   });
-// gbot.on('message', function (user, userID, channelID, message, evt) {
-//     if(message.indexOf(prefix) === 0 ) {
-//     }
-//     if (message.substring(0, 1) == '!') {
-//         var args = message.substring(1).split(' ');
-//         var cmd = args[0];
-//         args = args.splice(1);
-//         switch(cmd) {
-//             case 'help':
-//                 gbot.sendMessage({
-//                     to: channelID,
-//                     message: "GBot only has `!roll #` command right now :(\nI'll have more soon though! :D"
-//                 });
-//                 break;
-//             case 'roll':
-//                 var number = Math.floor(Math.random()*args[0]);
-//                 gbot.sendMessage({
-//                     to: channelID,
-//                     message: number
-//                 });
-//                 break;
-//         }
-//     }
-// });
