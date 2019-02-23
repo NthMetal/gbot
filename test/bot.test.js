4a3ec1ba2372ec9c79b56ae2d07bcb2bee8f2f70
nthmetal <832529@gmail.com>
2019-02-23 14:34:32 -0500

    auth configuration, no gbot object testing

--- test/bot.test.js
+++ test/bot.test.js
@@ -3,10 +3,10 @@ var { assert } = require('chai');
 var sinon = require('sinon');
 
 //bot imports
-//var Discord = require('discord.io');
 var auth = require(`../src/_config/auth.js`);
 var Environment = require(`../src/_config/env.js`);
 var EventHandler = require('../src/commands/eventHandler.js');
+
 //var MongoClient = require('mongodb');
 
 describe('Testing GBotBot Initialization', function() {
@@ -71,20 +71,15 @@ describe('Testing GBotBot Initialization', function() {
 
   describe('Testing GBotBot Creation', function() {
 
-    it('should create an instance of GBot', function() {
+    it('should create a Discord object', function() {
         var Discord = require('discord.io');
         assert.isObject(Discord, 'discord object created');
-        var gbot = new Discord.Client({
-            token: auth.token,
-            autorun: true
-        });
-        assert.isObject(gbot, 'gbot object created');
-        assert.isFunction(gbot.on, 'gbot can listen to events');
-        
+        // var gbot = new Discord.Client({
+        //     token: auth.token,
+        //     autorun: true
+        // });
+        // assert.isObject(gbot, 'gbot object created');
+        // assert.isFunction(gbot.on, 'gbot can listen to events');
     })
-
-    // it('should successfully initialize the EventHandler', function() {
-    //     var eventHandler = new EventHandler();
-    // })
 
   });
