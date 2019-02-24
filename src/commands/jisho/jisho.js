930397b3b211eed1e6472c47f39d1f61ed7770d2
Lichaes <remove>
2019-02-23 19:49:56 -0600

    7\:47pm test commit after my failure \(but have fixed\!\)

--- src/commands/jisho/jisho.js
+++ src/commands/jisho/jisho.js
@@ -1,3 +1,6 @@
+const jishoApi = require('unofficial-jisho-api');
+
+
 module.exports = async (args, argument, data, messenger, db) => {
     var outcome = 
     messenger.textMessage(argument+ ' is ' +outcome, data.channel_id);
